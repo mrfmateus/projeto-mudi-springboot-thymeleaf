bc6581bcd1a9ef653e916e690a0527ad11499143
Mateus Ferreira <mrf.mateus@hotmail.com>
2023-05-10 00:20:22 -0300

    PedidoRepository and home navbar

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,26 +1,23 @@
 package br.com.alura.mvc.mudi.controller;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-  @PersistenceContext
-  private EntityManager entityManager;
+  @Autowired
+  private PedidoRepository pedidoRepository;
 
   @GetMapping("/home")
   public String home(Model model) {
-    Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-
-    List<Pedido> pedidosList = query.getResultList();
+    List<Pedido> pedidosList = pedidoRepository.findAll();
     model.addAttribute("pedidos", pedidosList);
     return "home";
   }
